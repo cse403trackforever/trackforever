e83c5075dc0bed4c15578e5f1ee191fdeba8f238
Christopher Addison <cjteam@cs.washington.edu>
2018-04-06 00:22:06 -0700

    [Add] server project setup

---
+++ server/src/test/java/ExampleJavaTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleJavaTest {
+    @Test
+    public void exampleTest() {
+        assertEquals("wassup", 5, 5);
+    }
+}
---
+++ server/src/test/kotlin/ExampleKotlinTest.kt
@@ -0,0 +1,8 @@
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class ExampleKotlinTest() {
+    @Test fun test() {
+        assertEquals(5, 5, "wassup")
+    }
+}

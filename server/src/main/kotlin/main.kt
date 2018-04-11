f9629254f576f3c4a032d2d541bf8d756c900718
Christopher Addison <cjteam@cs.washington.edu>
2018-04-10 22:32:36 -0700

    [Update] clean up googlecode plugin test

--- server/src/main/kotlin/main.kt
+++ server/src/main/kotlin/main.kt
@@ -4,6 +4,13 @@ import plugin.GoogleCodePlugin
 fun main(args: Array<String>) {
     println("test")
 
-    GitHubPlugin("cse403trackforever", "trackforever").import()
+    GitHubPlugin("cse403trackforever", "trackforever").import() // -> OUR STRUCT
     GoogleCodePlugin("migratordotnet").import()
+}
+
+fun factorial(n: Int): Int {
+    var result = 1
+    for (i in 2..n)
+        result *= i
+    return result
 }
--- server/src/main/kotlin/plugin/api/GoogleCodeDefaultAPI.kt
+++ server/src/main/kotlin/plugin/api/GoogleCodeDefaultAPI.kt
@@ -6,15 +6,12 @@ import java.net.URL
 class GoogleCodeDefaultAPI(projectName: String) : GoogleCodeAPI {
     private val archiveUrlTemplate = "https://storage.googleapis.com/google-code-archive/v2/code.google.com/$projectName"
 
-    override fun fetchProject(): InputStream {
-        return URL("$archiveUrlTemplate/project.json").openStream()
-    }
+    override fun fetchProject(): InputStream
+            = URL("$archiveUrlTemplate/project.json").openStream()
 
-    override fun fetchIssuesPage(pageNum: Int): InputStream {
-        return URL("$archiveUrlTemplate/issues-page-$pageNum.json").openStream()
-    }
+    override fun fetchIssuesPage(pageNum: Int): InputStream
+            = URL("$archiveUrlTemplate/issues-page-$pageNum.json").openStream()
 
-    override fun fetchIssue(issueId: Int): InputStream {
-        return URL("$archiveUrlTemplate/issues/issue-$issueId.json").openStream()
-    }
+    override fun fetchIssue(issueId: Int): InputStream
+            = URL("$archiveUrlTemplate/issues/issue-$issueId.json").openStream()
 }

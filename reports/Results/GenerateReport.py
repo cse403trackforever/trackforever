5d41380add50e2f527cfcfdf08361b8d45962224
William Cao <pastor13@uw.edu>
2018-05-30 23:27:21 -0700

    [Update] Week 10 Updated CSVs, Scripts, and Reports

--- reports/Results/GenerateReport.py
+++ reports/Results/GenerateReport.py
@@ -24,8 +24,7 @@ def scoreGoogleCode(tex_document, relative_path, issue_tracker_name, project_nam
 		tex_document += code_score.get_graph()
 		code_score.reset()
 
-scoreGeneric(texdoc, ['GitHub', 'Project 1'], 'GitHub', 'Project 1')
-scoreGeneric(texdoc, ['GitHub', 'Project 2'], 'GitHub', 'Project 2')
+scoreGeneric(texdoc, ['GitHub', 'Microsoft Dotnet'], 'GitHub', 'Microsoft Dotnet')
 scoreGeneric(texdoc, ['Google Code', 'Appscale'], 'Google Code', 'Appscale')
 scoreGeneric(texdoc, ['Google Code', 'Openbookproject'], 'Google Code', 'Openbookproject')
 scoreGeneric(texdoc, ['Redmine', 'Project 1'], 'Redmine', 'Project 1')
